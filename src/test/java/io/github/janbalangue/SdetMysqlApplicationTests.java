63cd1aec4e30485daeb5c0321d1532fcbfda43ab
Jan_Balangue <jan.a.balangue@gmail.com>
2022-08-18 19:39:37 -0500

    first commit

---
+++ src/test/java/io/github/janbalangue/SdetMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.janbalangue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdetMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
